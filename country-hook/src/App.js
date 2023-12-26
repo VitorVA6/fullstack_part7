5c979afb22a4e687e991a4fa8b2a413d5b613284
Vitor Vaz Andrade <vitorandrade@ecomp.uefs.br>
2023-12-26 10:37:21 -0300

    feat: adding ultimate-hooks project

--- country-hook/src/App.js
+++ country-hook/src/App.js
@@ -18,7 +18,27 @@ const useField = (type) => {
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect(() => {})
+  useEffect(() => {
+    if(name === ''){
+      setCountry(null)
+      return
+    }
+    axios.get(`https://studies.cs.helsinki.fi/restcountries/api/name/${name}`)
+      .then(response => {
+        setCountry({
+          found: true,
+          data: {
+            name: response.data.name.common,
+            capital: response.data.capital[0],
+            population: response.data.population,
+            flag: response.data.flags.png
+          }
+        })
+      })
+      .catch(err => {
+        setCountry({ found: false })
+      })
+  }, [name])
 
   return country
 }
